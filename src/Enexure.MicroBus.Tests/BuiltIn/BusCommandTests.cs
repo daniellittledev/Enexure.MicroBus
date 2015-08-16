570eb1c6d98300560df13e46a217a314f4494d70
Daniel Little <lavinski@live.com.au>
2015-08-16 13:46:41 +1000

    Moved non IOC tests

---
+++ src/Enexure.MicroBus.Tests/BuiltIn/BusCommandTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Enexure.MicroBus;
+using Enexure.MicroBus.Tests.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Enexure.MicroBus.Tests
+{
+	[TestFixture]
+	public class CommandTests
+	{
+		class Command : ICommand { }
+
+		class CommandHandler : ICommandHandler<Command>
+		{
+			public Task Handle(Command command)
+			{
+				return Task.FromResult(0);
+			}
+		}
+
+		[Test]
+		public void NoHandlerShouldThrow()
+		{
+			var bus = new BusBuilder()
+				.BuildBus();
+
+			var func = (Func<Task>)(() => bus.Send(new Command()));
+
+			func.ShouldThrowExactly<NoRegistrationForMessageException>().WithMessage("No registration for message of type Command was found");
+		}
+
+		[Test]
+		public async Task TestCommand()
+		{
+			var pipline = new Pipeline()
+				.AddHandler<PipelineHandler>();
+
+			var bus = new BusBuilder()
+				.RegisterCommand<Command>().To<CommandHandler>(pipline)
+				.BuildBus();
+
+			await bus.Send(new Command());
+
+		}
+
+		[Test]
+		public void TestMissingCommand()
+		{
+			var bus = new BusBuilder()
+				.BuildBus();
+
+			new Func<Task>(() => bus.Send(new Command()))
+				.ShouldThrow<NoRegistrationForMessageException>();
+
+		}
+	}
+}
