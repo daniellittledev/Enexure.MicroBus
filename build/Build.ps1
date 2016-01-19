ee1d27a5618ace734085cd7f657b33463880d3cd
Daniel Little <lavinski@live.com.au>
2016-01-19 20:51:52 +1000

    Do the dnvm stuff at install time, and do a dnu restore

--- build/Build.ps1
+++ build/Build.ps1
@@ -34,13 +34,7 @@ task Test -depends Compile {
 
 task Compile { 
 
-	nuget sources add -Name "api.nuget.org" -Source "https://api.nuget.org/v3/index.json"
-	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))
-	dnvm update-self
-	dnvm upgrade
-	dnvm install 1.0.0-rc1-final
-	dnvm list
-	dnvm use 1.0.0-rc1-final
+	dnu restore
 
 	$projects = ls "$solutionDir\src"
 
